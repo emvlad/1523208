a295860a7113371354af050d630b1b1c4995b31b
Vladimir Chris <cevchris23@outlook.com>
2018-08-23 15:50:46 -0400

    Atelier No.1 - Ajout Etiquette Bonjour

--- app/src/main/java/ca/cours5b5/vladimirchrisphonte/Parametres.java
+++ app/src/main/java/ca/cours5b5/vladimirchrisphonte/Parametres.java
@@ -2,6 +2,8 @@ package ca.cours5b5.vladimirchrisphonte;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
 
 public class Parametres extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class Parametres extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parametres);
+
+        Log.d("MonEtiquette", "Bonjour!");
+
     }
 }
