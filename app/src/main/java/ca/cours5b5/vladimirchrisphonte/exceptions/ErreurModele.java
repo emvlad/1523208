5946f446af81861185ac980caa54aa5521f2011b
Vladimir Chris <cevchris23@outlook.com>
2018-10-25 14:20:28 -0400

    ajout de dossiers donnnees et mis à jour d'exceptions

---
+++ app/src/main/java/ca/cours5b5/vladimirchrisphonte/exceptions/ErreurModele.java
@@ -0,0 +1,10 @@
+package ca.cours5b5.vladimirchrisphonte.exceptions;
+
+public class ErreurModele extends RuntimeException{
+
+    public ErreurModele(Exception e) {super(e);}
+
+    public ErreurModele(String message){
+        super(message);
+    }
+}
---
+++ app/src/main/java/donnees/SourceDeDonnees.java
@@ -0,0 +1,10 @@
+package donnees;
+
+import java.util.Map;
+
+public abstract class SourceDeDonnees {
+
+    public abstract Map<String, Object> chargerModele(final String cheminSauvegarde);
+
+    public abstract void sauvegarderModele(final String cheminSauvegarde, final Map<String, Object> objetJson);
+}
